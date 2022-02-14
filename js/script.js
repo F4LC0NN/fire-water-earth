3ac27bfb6757cb9712b6d993a402fa1ea2300b62
F4LC0NN <thomasruben25@gmail.com>
2022-02-14 08:59:59 +0100

    Create scores, round & result variables to update them later

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+--------------------
+FIRE-WATER-EARTH
+--------------------
+*/
+
+/*
+--------------------
+Initialization
+--------------------
+*/
+
+// Create a player win count
+let playerScore = 0;
+
+// Create a computer win count
+let computerScore = 0;
+
+// Create a rounds count
+let rounds = 0;
+
+// Create a result variable with the type string
+let result = '';
