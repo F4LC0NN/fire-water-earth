70429fc36bbaad5b1fc773fbdb383706deaeed86
F4LC0NN <thomasruben25@gmail.com>
2022-02-14 09:02:09 +0100

    Make the computer choose a move randomly

--- js/script.js
+++ js/script.js
@@ -21,3 +21,16 @@ let rounds = 0;
 
 // Create a result variable with the type string
 let result = '';
+
+/*
+--------------------
+Algorithms
+--------------------
+*/
+
+// Create a function named computerPlay() that chooses a random move
+function computerPlay() {
+  // Create an array named moves and set three elements FIRE, WATER & EARTH
+  let moves = ['FIRE', 'WATER', 'EARTH'];
+  return moves[Math.floor(Math.random() * moves.length)];
+}
